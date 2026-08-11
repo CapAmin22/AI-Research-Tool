0ad3f8e4940bea6fcc9421f2c575177788e7ad3b
CapAmin22 <shaikhaminrehman@gmail.com>
2026-08-11 18:59:11 +0530

    chore: remove leaked Groq API keys

--- set_groq.py
+++ set_groq.py
@@ -4,7 +4,7 @@ data = {}
 if os.path.exists(path):
     with open(path, 'r') as f:
         data = json.load(f)
-data['groq-key'] = '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'
+data['groq-key'] = 'YOUR_GROQ_API_KEY_HERE'
 os.makedirs(os.path.dirname(path), exist_ok=True)
 with open(path, 'w') as f:
     json.dump(data, f, indent=4)
